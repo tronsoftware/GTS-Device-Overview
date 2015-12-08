c8c406b7424557a4d3a5b3e5c21907e418ad94b0
Hernan Diaz <tron@3tech.com.ar>
2015-12-08 19:43:05 -0300

    Update index.php
    jquery auto refresco

--- index.php
+++ index.php
@@ -85,5 +85,13 @@
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-  </body>
+<script>
+ function autoRefresh_div()
+ {
+      $("#tabla").load(location.href + " #tabla");
+  }
+ 
+  setInterval('autoRefresh_div()', 5000); // refresh div after 5 secs
+</script>
+</body>
 </html>
